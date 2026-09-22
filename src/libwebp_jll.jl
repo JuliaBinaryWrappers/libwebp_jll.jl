e79122e7f7f40641b5ad7acfec7ec70142362c52
jlbuild <juliabuildbot@gmail.com>
2026-09-22 20:01:47 +0000

    libwebp_jll build 1.6.1+0

--- src/libwebp_jll.jl
+++ src/libwebp_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libwebp")
-JLLWrappers.@generate_main_file("libwebp", UUID("c5f90fcd-3b7e-5836-afba-fc50a0988cb2"))
+JLLWrappers.@generate_main_file("libwebp", Base.UUID("c5f90fcd-3b7e-5836-afba-fc50a0988cb2"))
 end  # module libwebp_jll
